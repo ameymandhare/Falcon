18786a307029a8179c6daff86bebb9672d6f9b72
ameymandhare <ameymandhare@gmail.com>
2020-05-09 13:47:30 +0530

    Cleanup don, Include only jquery

--- FalconSchool/App_Start/BundleConfig.cs
+++ FalconSchool/App_Start/BundleConfig.cs
@@ -8,29 +8,27 @@ namespace FalconSchool
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
+
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          ));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js", 
-                        "~/Scripts/jquery-ui-1.12.1.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/moment.js"));
+                        "~/Content/Scripts/jquery-1.12.4.min.js",
+                        "~/Content/Scripts/jquery-ui-1.12.1.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/themes/base/jquery-ui.min.css",
-                      "~/Content/bootstrap-datetimepicker-build.less",
-                      "~/Content/_bootstrap-datetimepicker.less",
-                      "~/Content/bootstrap/variables.less"));
+                      "~/Content/jQueryThemes/base/jquery-ui.css",
+                      "~/Content/jQueryThemes/base/all.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/custom-scripts").Include(
+                    "~/Content/Scripts/ProspectStudent/CreateProspect.js"));
         }
     }
 }
